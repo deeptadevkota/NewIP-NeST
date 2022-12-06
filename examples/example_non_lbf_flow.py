83bb266ced397a9f011ce4ecd607ca59f317ab8f
deeptadevkota <deeptadevkota21@gmail.com>
2022-12-06 16:01:41 +0530

    comments-added

--- examples/example_non_lbf_flow.py
+++ examples/example_non_lbf_flow.py
@@ -21,10 +21,15 @@ class NewIP:
         self.non_lbf_flows.append(copy.deepcopy(non_lbf_flow))
 
 
+
 newip_obj = NewIP()
+
+# setting up the Topology
 newip_obj.create_topology()
 
 
+
+# Defining New-IP contract less flows
 flow1 = NonLbfFlow(src_node=newip_obj.topo.h1, dst_node=newip_obj.topo.h3,
                    src_addr_type="ipv4", dst_addr_type="ipv6", pkt_count=5)
 flow2 = NonLbfFlow(src_node=newip_obj.topo.h1, dst_node=newip_obj.topo.h2,
@@ -34,12 +39,14 @@ flow3 = NonLbfFlow(src_node=newip_obj.topo.h1, dst_node=newip_obj.topo.h3,
 flow4 = NonLbfFlow(src_node=newip_obj.topo.h1, dst_node=newip_obj.topo.h2,
                    src_addr_type="ipv6", dst_addr_type="ipv6", pkt_count=5)
 
-# NewIP contract less flows
+# adding NewIP contract less flows to the NewIP class
 newip_obj.add_non_lbf_flows(flow1)
 newip_obj.add_non_lbf_flows(flow2)
 newip_obj.add_non_lbf_flows(flow3)
 newip_obj.add_non_lbf_flows(flow4)
 
+
+# instantiating the Experiment class of NeST
 exp = Experiment(
     name="non-lbf-flow-experiment",
     non_lbf_flows=newip_obj.non_lbf_flows,
@@ -48,11 +55,12 @@ exp = Experiment(
 
 
 
-
+# Defining UDP flow from h1 to h3
 flow5 = Flow(source_node=newip_obj.topo.h1, destination_node=newip_obj.topo.h3,
              destination_address=newip_obj.topo.info_dict[newip_obj.topo.h3.name]['ipv4'], 
              start_time=0, stop_time=10, number_of_streams=1)
 
+# Defining TCP BBR flow from h1 to h2
 flow6 = Flow(source_node=newip_obj.topo.h1, destination_node=newip_obj.topo.h2,
              destination_address=newip_obj.topo.info_dict[newip_obj.topo.h2.name]['ipv4'], 
              start_time=0, stop_time=10, number_of_streams=1)
@@ -64,4 +72,5 @@ exp.add_udp_flow(flow5, target_bandwidth="12mbit")
 # Adding TCP flow
 exp.add_tcp_flow(flow6, "bbr")
 
+# Running the experiment for all added flows
 exp.run()
